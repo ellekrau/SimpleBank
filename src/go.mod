2bc825336da19f6162949a90ac9a417b64d0d45c
ellekrau <ellekrau91@gmail.com>
2022-03-27 22:59:21 -0300

    Add postgres driver to main_test.go and get github.com/lib/pq

--- src/go.mod
+++ src/go.mod
@@ -1,3 +1,5 @@
 module github.com/ellekrau/SimpleBank
 
 go 1.18
+
+require github.com/lib/pq v1.10.4
---
+++ src/sqlc/main_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://admin:P@ssw0rd@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database!")
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
