a4e5d6d389543481f04f34ecd96209b2eadc3689
ellekrau <ellekrau91@gmail.com>
2022-03-27 23:57:41 -0300

    Add more validations to CreateAccount test

--- src/sqlc/account_test.go
+++ src/sqlc/account_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"github.com/ellekrau/SimpleBank/utils/enum/currency"
 	test "github.com/stretchr/testify/require"
 	"testing"
 )
@@ -10,14 +11,18 @@ func TestCreateAccount(t *testing.T) {
 	args := CreateAccountParams{
 		Owner:    "Jaine",
 		Balance:  1000,
-		Currency: "REAL",
+		Currency: currency.Real,
 	}
 
 	account, err := testQueries.CreateAccount(context.Background(), args)
+
 	test.NoError(t, err)
 	test.NotEmpty(t, account)
 
 	test.Equal(t, args.Owner, account.Owner)
 	test.Equal(t, args.Balance, account.Balance)
 	test.Equal(t, args.Currency, account.Currency)
+
+	test.NotZero(t, account.ID)
+	test.NotZero(t, account.CreatedAt)
 }
